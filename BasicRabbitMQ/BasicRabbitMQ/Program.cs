60f846d907f5e97b974948e22bc5b954a2ea743f
Seher CEVIK <scevik@smartpulse.io>
2025-03-10 13:53:19 +0300

    Project added

---
+++ BasicRabbitMQ/BasicRabbitMQ/Program.cs
@@ -0,0 +1,50 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+namespace Send;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri("amqp://guest:guest@localhost:5672");
+
+            var connection = factory.CreateConnection();
+            Console.WriteLine("Bağlantı başarılı!");
+
+            var channel = connection.CreateModel();
+            channel.QueueDeclare("messageQueue", true, false, false);
+
+            //QueueDeclare, eğer belirtilen adla bir kuyruk yoksa yeni bir tane oluşturur.
+            //"messageQueue" → Kuyruğun adı
+            //Parametreler:
+            //true → Kalıcı bir kuyruk(mesajlar RabbitMQ yeniden başlatıldığında kaybolmaz).
+            //false → Bu kuyruk sadece bu bağlantıya özel değil, başka bağlantılar da kullanabilir.
+            //false → Eğer kuyruk boşsa otomatik silinmez.
+
+            var message = "deneme birkiii";
+            var body = Encoding.UTF8.GetBytes(message);
+
+            channel.BasicPublish(exchange: "",
+                                 routingKey: "messageQueue",
+                                 basicProperties: null,
+                                 body: body);
+
+            channel.WaitForConfirmsOrDie();
+            //exchange: "" → Doğrudan kuyruğa mesaj gönderildiğini belirtir.
+            //routingKey: "messageQueue" → Mesajın "messageQueue" adlı kuyruğa gitmesini sağlar.
+            //basicProperties: null → Ekstra bir özellik eklenmemiş.
+            //body: body → Gönderilecek mesajın içeriği.
+
+            Console.WriteLine("Mesaj gönderildi.");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Hata: {ex.Message}");
+        }
+
+    }
+}
---
+++ BasicRabbitMQ/CaptureMessage/Program.cs
@@ -0,0 +1,50 @@
+﻿
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+internal class Program
+{
+    //RabbitMQ Consumer(Tüketici) uygulamasıdır."messageQueue" kuyruğundaki mesajları dinleyip almak için kullanılır.
+    static void Main(string[] args)
+    {
+        try
+        {
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri("amqp://guest:guest@localhost:5672");
+
+            using var connection = factory.CreateConnection();
+
+            var channel = connection.CreateModel();
+            channel.QueueDeclare("messageQueue", true, false, false);
+
+            //kuyruktan mesajı alma
+            var consumer = new EventingBasicConsumer(channel);
+            channel.BasicConsume(queue: "messageQueue",
+                                 autoAck: true,
+                                 consumer: consumer);
+            //         queue =>   Dinlenecek kuyruğun adı
+            //                AutoAck =>  Mesajın otomatik olarak onaylanıp onaylanmayacağını belirler.
+            //true olursa, mesaj alındığında RabbitMQ'ya "işlem tamam" bilgisi otomatik gider ve mesaj silinir.
+            //false olursa, tüketici(consumer) mesajı işlediğini elle bildirmelidir(BasicAck ile).
+            //                consumer => RabbitMQ'dan gelen mesajları alacak ve işleyecek tüketici nesnesidir (EventingBasicConsumer).
+            
+
+            //consumer ı dinlemek
+            //Tüketici (consumer), kuyruktan yeni mesaj geldiğinde Consumer_Received metodunu çalıştıracak.
+            consumer.Received += Consumer_Received;
+
+            Console.ReadLine();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Hata: {ex.Message}");
+        }
+    }
+
+    private static void Consumer_Received(object? sender, BasicDeliverEventArgs e)
+    {
+        //yakalayacağımız veriyi binary den string e çevirme
+        Console.WriteLine("Gelen Mesaj: " + Encoding.UTF8.GetString(e.Body.ToArray()));
+    }
+}
